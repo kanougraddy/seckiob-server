bbe92e05d619b08e95dbe9eb175e15cdba94a862
kanougraddy <kanougraddy@gmail.com>
2025-09-11 11:27:24 +0100

    Delete providers directory

--- providers/mobilemoney.js
+++
@@ -1,8 +0,0 @@
-export async function processMobileMoney(payment){
-  const provider = (payment.provider || 'mock').toLowerCase();
-  if (provider === 'mock'){
-    return { ok: true, ref: 'MM-' + payment.id, message: 'mock_initiated' };
-  }
-  // TODO: integrate MTN/Airtel real providers here
-  return { ok: false, ref: null, message: 'provider_not_implemented' };
-}
